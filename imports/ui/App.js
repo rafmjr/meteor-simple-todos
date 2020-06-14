0a5ed3c80e7ae0fbdcf728b08ce9fa9be0c5db7e
Rafael Mejia <rafamr.7@gmail.com>
2020-06-13 21:03:45 -0400

    Continued working on sections 4, 5 and 6
    4. Forms and events
    5. Update and remove
    6. Running on mobile
    
    I'm amaze by how this frameworks integrates well with Mongo. I also
    think is cool the possibility of coding both a website and mobile app
    at the same time, this could save tons of times for one of the projects
    I have on mind.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks';
@@ -7,33 +8,47 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
+    handleSubmit(event) {
+        event.preventDefault();
 
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    renderTasks() {
+        return this.props.tasks.map((task) => (
+            <Task key={task._id} task={task} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+                <form
+                    className="new-task"
+                    onSubmit={this.handleSubmit.bind(this)}
+                >
+                    <input
+                        type="text"
+                        ref="textInput"
+                        placeholder="Type to add new tasks"
+                    />
+                </form>
+                <ul>{this.renderTasks()}</ul>
+            </div>
+        );
+    }
 }
 
-export default withTracker(() => ({ tasks: Tasks.find({}).fetch() }))(App);
+export default withTracker(() => ({
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+}))(App);
